e35cbadece7c46d57006f3a5e5f60f455829f668
Mathanraj <131568872+Matha54@users.noreply.github.com>
2023-12-17 02:18:23 -0800

    Add files via upload

---
+++ day 22/src/App.js
@@ -0,0 +1,19 @@
+import Dashboard from "./components/Dashboard";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return  <>
+     <div id="wrapper">
+      <Sidebar/>
+      <div id="content-wrapper" className="d-flex flex-column">
+      <div id="content">
+      <Dashboard/>
+
+
+      </div>
+      </div>
+      </div> 
+      </>
+}
+
+export default App;
